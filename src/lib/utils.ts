92fd8fd49d8f67612361da3f0ad15dffb3d17194
Achyut875 <achyutpandey51513@gmail.com>
2025-08-25 01:22:47 +0530

    Fix cn utility function - add clsx and tailwind-merge dependencies

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,3 +1,10 @@
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
 export const forceDownload = (url: string, filename: string) => {
   const link = document.createElement('a');
   link.href = url;
